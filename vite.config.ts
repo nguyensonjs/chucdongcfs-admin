bb6a2b3ea7011727b21b43007352a80062f94cd7
sonhn <nhson.codes@gmail.com>
2025-08-31 10:19:13 +0700

    Thêm cấu hình ssl cho local

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,26 @@
 import { reactRouter } from '@react-router/dev/vite'
 import tailwindcss from '@tailwindcss/vite'
+import fs from 'fs'
+import path from 'path'
 import { defineConfig } from 'vite'
 import devtoolsJson from 'vite-plugin-devtools-json'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   css: { devSourcemap: true },
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), devtoolsJson()]
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), devtoolsJson()],
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'cert/admin.chucdongcfs.com-key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'cert/admin.chucdongcfs.com.pem'))
+    },
+    allowedHosts: ['admin.chucdongcfs.com'],
+    port: 4443,
+    host: 'admin.chucdongcfs.com',
+    hmr: {
+      path: '/vite/'
+    },
+    sourcemapIgnoreList: false,
+    open: 'https://admin.chucdongcfs.com:4443'
+  }
 })
